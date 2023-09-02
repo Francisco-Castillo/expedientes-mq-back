66af3da90c25977f018d0b521b840cddae1ce152
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2023-09-02 20:33:33 -0300

    commit inicial

---
+++ src/main/java/ar/com/mq/expedientes/ExpedientesServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.mq.expedientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpedientesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpedientesServiceApplication.class, args);
+	}
+
+}
