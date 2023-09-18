f0a7860b5118fd26df00710a27e24bc176449a87
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2023-09-18 12:37:57 -0300

    se agrega cors para localhost react en controlador de expedientes

--- src/main/java/ar/com/mq/expedientes/api/web/ExpedienteController.java
+++ src/main/java/ar/com/mq/expedientes/api/web/ExpedienteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/expedientes")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @Slf4j
 public class ExpedienteController {
 
