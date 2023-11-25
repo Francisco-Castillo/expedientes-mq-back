39e27af9cc6021d767ead24d1776ac365f405e19
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2023-11-25 17:10:29 -0300

    add cors filter

--- src/main/java/ar/com/mq/expedientes/Application.java
+++ src/main/java/ar/com/mq/expedientes/Application.java
@@ -2,6 +2,10 @@ package ar.com.mq.expedientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://expedientes-mq-front-m0qsu3smm-francisco-castillos-projects.vercel.app/"); // Reemplaza con tu dominio permitido
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
