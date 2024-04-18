444c72d8833960d355c47a55761fa9eb3b88e93d
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2024-04-18 18:30:08 -0300

    fix cors

--- src/main/java/ar/com/mq/expedientes/api/config/CORSFilter.java
+++ src/main/java/ar/com/mq/expedientes/api/config/CORSFilter.java
@@ -1,6 +1,8 @@
 package ar.com.mq.expedientes.api.config;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,21 +18,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class CORSFilter implements Filter {
 
+	private final List<String> allowedOrigins = Arrays.asList("http://vps-4020997-x.dattaweb.com",
+			"http://localhost:5173");
+
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://vps-4020997-x.dattaweb.com");
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Accept, X-Requested-With, x-customer-header-1, x-customer-header-2");
 
-		chain.doFilter(req, res);
+		String origin = request.getHeader("Origin");
+
+		if (allowedOrigins.contains(origin)) {
+                        response.setHeader("Access-Control-Allow-Origin", origin);			
+                        response.setHeader("Access-Control-Allow-Credentials", "true");
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers",
+					"Content-Type, Accept, X-Requested-With, x-customer-header-1, x-customer-header-2");
+		}
+
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);
+		}
 	}
 
 	@Override
