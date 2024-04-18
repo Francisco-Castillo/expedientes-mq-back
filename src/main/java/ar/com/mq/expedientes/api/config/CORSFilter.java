a8b590616919f6be70804b6e4d73a48b5e9b3537
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2024-04-18 16:04:22 -0300

    agrega cors para localhost

--- src/main/java/ar/com/mq/expedientes/api/config/CORSFilter.java
+++ src/main/java/ar/com/mq/expedientes/api/config/CORSFilter.java
@@ -23,6 +23,7 @@ public class CORSFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "http://vps-4020997-x.dattaweb.com");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
